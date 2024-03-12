01ac680cd12e85a4e575384a7fc0604ea11200c3
Dheeraj Sharma <129324009+theedheeraj@users.noreply.github.com>
2024-03-12 00:37:56 +0000

    learn about data types

--- .devcontainer/01_basics/02_dataTypes..js
+++ .devcontainer/01_basics/02_dataTypes..js
@@ -2,6 +2,28 @@
 
 // alert( 3 + 3 ) // We are using nodeJS , not browser
 
-console.log(3 + 3)
+console.log(3 + 3 + 3*3)
 
-console.log("DheerajSharma")
+console.log("Dheeraj Sharma")
+
+let name = "Dheeraj Sharma"
+let age = 18
+let isLoggedIn = false
+
+// number => 2 to power 53
+//bigint 
+// string => ""
+//boolean => true/false
+//null => standalone value or emmpty value 
+//undefined =>
+//symbol => unique 
+
+
+//object 
+
+console.log(typeof"Dheeraj")// output string
+console.log(typeof age )//output number
+console.log(typeof null )//output object
+console.log(typeof undefined)//output undefined
+
+
