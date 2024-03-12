50bf45093632ca59b074d324e66358e2c7862c8a
Dheeraj Sharma <129324009+theedheeraj@users.noreply.github.com>
2024-03-12 01:12:14 +0000

    conversion of data type

---
+++ .devcontainer/01_basics/03_conversionOperation.js
@@ -0,0 +1,23 @@
+let score = 33// type will be number if it is in quote it will be string
+
+
+console.log(typeof score);
+console.log(typeof (score));
+
+let valueInNuber = Number(score)
+console.log(typeof valueInNuber);
+console.log(valueInNuber);
+
+
+// "33" => 33
+// "33abc" NaN
+// true => 1 
+
+let isLoggedIn = 1
+
+let booleanisLoggedIn = boolean(isLoggedIn);
+console.log(booleanisLoggedIn);
+
+// 1 => true , 0 => false
+// " " => false
+// "Dheeraj" => true
