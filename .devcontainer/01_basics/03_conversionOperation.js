4e410c62bdec109e43ca515f2e874b5883598bf4
Dheeraj Sharma <129324009+theedheeraj@users.noreply.github.com>
2024-03-12 01:36:06 +0000

    learn prefix and post fix

--- .devcontainer/01_basics/03_conversionOperation.js
+++ .devcontainer/01_basics/03_conversionOperation.js
@@ -21,3 +21,34 @@ console.log(booleanisLoggedIn);
 // 1 => true , 0 => false
 // " " => false
 // "Dheeraj" => true
+
+let value = 3
+let negValue =  -value
+console.log(negValue)
+
+console.log(2+2)
+console.log(2-2)
+console.log(2*2)
+console.log(2/2)
+console.log(2%2)
+
+
+let str1 = "Dheeraj "
+let str2 = "Sharma "
+
+let str3 = str1 + str2
+console.log(str3)
+
+console.log("1" + 2);
+console.log( 1 + "2");
+console.log("1" + 2 + 2 );
+console.log( 1 + 2 + "2");
+
+console.log(+true)//1
+console.log(+"")//0
+
+
+let gamecounter  = 100
+++gamecounter
+console.log(gamecounter)
+
