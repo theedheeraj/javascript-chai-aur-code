9352a194d179d85620783fec002e3b8205b7f56d
Dheeraj Sharma <129324009+theedheeraj@users.noreply.github.com>
2024-03-12 02:22:21 +0000

    datatype summmary

---
+++ .devcontainer/01_basics/datatypes-summary.js
@@ -0,0 +1,39 @@
+// primitive
+
+// 7 types : string , number , boolean , null , undefined , symbol ,Bigint
+
+// dynamic langauge
+
+const score = 100
+const scorevalue = 100.3
+
+const isloogedin = false
+const outsidetemp = null
+
+let userEmail;
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+
+console.log(id == anotherId) // fasle
+
+const bigNumber = 5623899721687521387516n
+
+console.log(typeof bigNumber)
+
+
+// reference type (non - primitive )
+
+// Array , objects , functions 
+
+const heros = ["shaktiman", "nagraj","doga"]
+
+let myObj{
+    name: "Dheeraj",
+    age:22,
+}
+
+const myFunction = function(){
+    console.log("Hello World");
+}
+//time 01:56:39
